8a786a32aafdf48fa5db08b6ff56f6c7da8aee65
webdevelopersinkenya <mirr22006@byui.edu>
2025-05-05 17:11:01 +0300

    Changes

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name?.");
+        string firstname = Console.ReadLine();
+
+        Console.Write("What is your second Name?");
+        string secondname = Console.ReadLine();
+        Console.WriteLine($"Your name is {secondname}, {firstname}, {secondname}");
     }
 }
